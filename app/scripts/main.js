703add07d958fdf30c61ab6ff6d37d7f20479bdd
Christopher Sahmi <maknshi57@hotmail.fr>
2022-04-18 11:36:42 +0200

    lalala

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 
-	// Anchor click change css overflow
+	/**
+		* GLOBAL
+		* Anchor click change css body overflow
+		* Anchors scroll to top animation
+		* Anchor click display WEB DEVELOPER hide others
+		* Anchor click display GAMER hide others
+		* Anchor click display PAINTER hide others
+		* Carousel Item Img
+		* Init Isotope
+		* filters functions
+		* Image loaded
+		* Go top on reload
+		* Effect Switch filters
+	*/
+
+	// Anchor click change css body overflow
 	$('.anchor').click(function() {
 		$('body').css({
 			'height' : 'auto',
@@ -8,11 +23,11 @@ $(document).ready(function() {
 		});
 	});
 	
-	// Anchors scroll animation
+	// Anchors scroll to top animation
 	$(document).on('click', 'a[href^="#"]', function (event) {
 		event.preventDefault();
 		$('html, body').animate({
-			scrollTop: $($.attr(this, 'href')).offset().top - 30
+			scrollTop: $($.attr(this, 'href')).offset().top - 0
 		}, 500);
 	});
 
@@ -24,17 +39,29 @@ $(document).ready(function() {
 			'overflow' : 'hidden',
 			'padding-top' : '0px',
 		});
+		$('#painter').css({
+			'opacity' : '0',
+			'height' : '0',
+			'overflow' : 'hidden',
+			'padding-top' : '0px',
+		});
 		$('#web-developer').css({
 			'opacity' : '1',
 			'height' : '100%',
-			'overflow' : 'auto',
-			'padding-top' : '50px',
+			'overflow' : 'unset',
+			'padding-top' : '70px',
 		});
 	});
 
-	// Anchor click display WEB DEVELOPER hide others
+	// Anchor click display GAMER hide others
 	$('a[href^="#gamer"]').click(function() {
 		$('#web-developer').css({
+			'opacity' : '0',
+			'height' : '0',
+			'overflow' : 'hidden',
+			'padding-top' : '0px',
+		});
+		$('#painter').css({
 			'opacity' : '0',
 			'height' : '0',
 			'overflow' : 'hidden',
@@ -43,29 +70,100 @@ $(document).ready(function() {
 		$('#gamer').css({
 			'opacity' : '1',
 			'height' : '100%',
-			'overflow' : 'auto',
-			'padding-top' : '50px',
+			'overflow' : 'unset',
+			'padding-top' : '70px',
 		});
 	});
 
-	// Img carousel
-	$(document).ready(function(){
-		$('.img').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			speed: 1000,
-			autoplay: true,
-			dots: false,
-			fade: true,
-			arrows: false,
-			infinite: true,
-			rows: 0,
-			slide: 'img',
-			adaptiveHeight: true,
-			pauseOnHover: true,
+	// Anchor click display PAINTER hide others
+	$('a[href^="#painter"]').click(function() {
+		$('#web-developer').css({
+			'opacity' : '0',
+			'height' : '0',
+			'overflow' : 'hidden',
+			'padding-top' : '0px',
+		});
+		$('#gamer').css({
+			'opacity' : '0',
+			'height' : '0',
+			'overflow' : 'hidden',
+			'padding-top' : '0px',
+		});
+		$('#painter').css({
+			'opacity' : '1',
+			'height' : '100%',
+			'overflow' : 'unset',
+			'padding-top' : '70px',
 		});
 	});
+
+	// Carousel Item Img
+	if ($('.item .img')[0]) {
+		$('.item .img').slick({
+			autoplay: true,
+			autoplaySpeed: 3000,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			dots: false,
+			slide: 'img',
+			rows: 0,
+			fade: true,
+			speed: 1000,
+			infinite: true,
+			pauseOnHover: false,
+			adaptiveHeight: true,
+			cssEase: 'linear',
+		});
+	}
 	
+	// Init Isotope
+	let $grid = $('.grid').isotope({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		horizontalOrder: true,
+		masonry: {
+			columnWidth: '.grid-sizer',
+			gutter: '.gutter-sizer'
+		}
+	});
+
+	// filters functions
+	var filterFns = {};
+
+	$('.grid').isotope({filter: '.all'});
+
+	// bind filter button click
+	$('.filters').on('click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({ filter: filterValue });
+	});
+	// change is-checked class on buttons
+	$('.filters').each(function(i, buttonGroup) {
+		var $buttonGroup = $(buttonGroup);
+		$buttonGroup.on('click', 'button', function() {
+			$buttonGroup.find('.is-checked').removeClass('is-checked');
+			$(this).addClass('is-checked');
+		});
+	});
+
+	// Image loaded
+	$grid.imagesLoaded().progress( function() {
+		$grid.isotope('layout');
+	});
+
+	// Effect Switch filters
+	$('.filters').on('click', '.filterRight', function () {
+		$('.filters').removeClass('filterLeft filterCenter').addClass('filterRight');
+	});
+	$('.filters').on('click', '.filterLeft', function () {
+		$('.filters').removeClass('filterRight filterCenter').addClass('filterLeft');
+	});
+
+	$('.filters').on('click', '.filterCenter', function () {
+		$('.filters').removeClass('filterRight filterLeft').addClass('filterCenter');
+	});
+
 	// Go top on reload
 	window.onbeforeunload = function () {
 		window.scrollTo(0, 0);
