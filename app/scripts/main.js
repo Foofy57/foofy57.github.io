f600ae4bf49cb2994790a158cbd4fd802f954776
Christopher Sahmi <maknshi57@hotmail.fr>
2025-10-03 16:40:32 +0200

    lalaa

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,95 +1,201 @@
-// scroll to the top of the page
-barba.hooks.enter(() => {
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, DrawSVGPlugin);
 
-    window.scrollTo(0, 0);
-
+// ScrollSmoother
+const smoother = ScrollSmoother.create({
+  smooth: 1.5,
+  effects: true,
+  smoothTouch: 0.1,
+  ignoreMobileResize: true
 });
 
-barba.init({
+const anchorLinks = document.querySelectorAll('.mainHeader__nav a[href^="#"]');
 
-  transitions: [{
+anchorLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
 
-    name: 'transition',
+    const targetID = this.getAttribute('href');
+    const targetEl = document.querySelector(targetID);
 
-    leave(data) {
-      // gsap.fromTo('.head',0,{opacity:'0'},{opacity:'1'});
-      gsap.fromTo('.head',0.7,{y:'0'},{y:'-100%'});
+    if (targetEl) {
+      const offset = window.innerHeight;
+      const targetPosition = targetEl.offsetTop + offset;
 
-      gsap.fromTo('.content',0.7,{y:'0'},{y:'100%'});
-      // gsap.fromTo('.wipe',0.2,{y:"100%",duration:1,stagger:0.1},{y:"0%",duration:1,stagger:0.1});
-
-      return gsap.from(data.current.container,0.7, {
-        
-      });
-    },
-
-    enter(data) {
-      // gsap.fromTo('.wipe',1,{y:"0",duration:1,stagger:0.1},{y:"-100%",duration:1,stagger:0.1});
-
-      gsap.fromTo('.head',0.7,{y:'-100%'},{y:'0'});
-
-      gsap.fromTo('.content',0.7,{y:'100%'},{y:'0'});
-
-      gsap.from(data.next.container,0.7, {
-        
+      smoother.scrollTo(targetPosition, {
+        duration: 2,
+        ease: "power2.inOut"
       });
     }
-  }]
-});
-
-
-document.addEventListener("keyup", function(event) {
-  if (event.keyCode == 84) {
-    gsap.fromTo('.head',0.7,{y:'0'},{y:'-100%'});
-    gsap.fromTo('.content',0.7,{y:'0'},{y:'100%'});
-    setTimeout(function() { 
-      window.location = 'team.html';
-    }, 700);
-  }
-});
-
-document.addEventListener("keyup", function(event) {
-  if (event.keyCode == 83) {
-    gsap.fromTo('.head',0.7,{y:'0'},{y:'-100%'});
-    gsap.fromTo('.content',0.7,{y:'0'},{y:'100%'});
-    setTimeout(function() { 
-      window.location = 'shop.html';
-    }, 700);
-  }
-});
-
-document.addEventListener("keyup", function(event) {
-  if (event.keyCode == 80) {
-    gsap.fromTo('.head',0.7,{y:'0'},{y:'-100%'});
-    gsap.fromTo('.content',0.7,{y:'0'},{y:'100%'});
-    setTimeout(function() { 
-      window.location = 'partners.html';
-    }, 700);
-  }
-});
-
-window.addEventListener("load", (event) => {
-  gsap.fromTo('.head',0.7,{y:'-100%'},{y:'0'});
-  gsap.fromTo('.content',0.7,{y:'100%'},{y:'0'});
-});
-
-
-
-
-var btnContainer = document.getElementById('menu__list');
-var btns = btnContainer.getElementsByClassName('menu__item');
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function() {
-    var current = document.getElementsByClassName('--is-active');
-
-    // If there's no active class
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(' --is-active', '');
-    }
-
-    // Add the active class to the current/clicked button
-    this.className += ' --is-active';
   });
-}
+});
+
+// Pin section bg
+$('.section').each(function () {
+  let section = this;
+  let bg = section.querySelector('.section__bg');
+  const viewportHeight = window.innerHeight;
+
+  if (bg) {
+    gsap.to(bg, {
+      scrollTrigger: {
+        trigger: section,
+        start: 'top top',
+        end: 'bottom+=' +  viewportHeight + 'px bottom',
+        scrub: true,
+        markers: false,
+        pin: bg,
+      }
+    });
+
+
+
+const span1 = bg.querySelectorAll('span:nth-child(1)');
+const span2 = bg.querySelectorAll('span:nth-child(2)');
+const span3 = bg.querySelectorAll('span:nth-child(3)');
+const span4 = bg.querySelectorAll('span:nth-child(4)');
+
+gsap.fromTo(
+  span1,
+  { opacity: 0, x: 400, rotate: 0 }, // départ
+  { 
+    opacity: 1,
+    x: 0,
+    scrollTrigger: {
+      trigger: section,
+      start: 'top top',
+      end: 'top+=' + viewportHeight + 'px top',
+      scrub: true,
+      markers: false,
+      onUpdate: self => {
+        const progress = self.progress; // de 0 à 1
+        span1.forEach(span1 => {
+          if (progress >= 0.3) {
+            const localProgress = (progress - 0.3) / 0.7; // 0 à 1 sur les 10% restants
+            span1.style.transform = `translateX(${gsap.getProperty(span1, "x")}px) rotate(${localProgress * -10}deg)`; 
+          }
+        });
+      }
+    }
+  }
+);
+
+gsap.fromTo(
+  span2,
+  { opacity: 0, x: -400, rotate: 0 }, // départ
+  { 
+    opacity: 1,
+    x: 0,
+    scrollTrigger: {
+      trigger: section,
+      start: 'top top',
+      end: 'top+=' + viewportHeight + 'px top',
+      scrub: true,
+      markers: false,
+      onUpdate: self => {
+        const progress = self.progress; // de 0 à 1
+        span2.forEach(span2 => {
+          if (progress >= 0.3) {
+            const localProgress = (progress - 0.3) / 0.7; // 0 à 1 sur les 10% restants
+            span2.style.transform = `translateX(${gsap.getProperty(span2, "x")}px) rotate(${localProgress * 0.5}deg)`; 
+          }
+        });
+      }
+    }
+  }
+);
+
+gsap.fromTo(
+  span3,
+  { opacity: 0, x: 400, rotate: 0 }, // départ
+  { 
+    opacity: 1,
+    x: 0,
+    scrollTrigger: {
+      trigger: section,
+      start: 'top top',
+      end: 'top+=' + viewportHeight + 'px top',
+      scrub: true,
+      markers: false,
+      onUpdate: self => {
+        const progress = self.progress; // de 0 à 1
+        span3.forEach(span3 => {
+          if (progress >= 0.3) {
+            const localProgress = (progress - 0.3) / 0.7; // 0 à 1 sur les 10% restants
+            span3.style.transform = `translateX(${gsap.getProperty(span3, "x")}px) rotate(${localProgress * 4}deg)`; 
+          }
+        });
+      }
+    }
+  }
+);
+
+gsap.fromTo(
+  span4,
+  { opacity: 0, x: -400, rotate: 0 }, // départ
+  { 
+    opacity: 1,
+    x: 0,
+    scrollTrigger: {
+      trigger: section,
+      start: 'top top',
+      end: 'top+=' + viewportHeight + 'px top',
+      scrub: true,
+      markers: false,
+      onUpdate: self => {
+        const progress = self.progress; // de 0 à 1
+        span4.forEach(span4 => {
+          if (progress >= 0.3) {
+            const localProgress = (progress - 0.3) / 0.7; // 0 à 1 sur les 10% restants
+            span4.style.transform = `translateX(${gsap.getProperty(span4, "x")}px) rotate(${localProgress * -10}deg)`; 
+          }
+        });
+      }
+    }
+  }
+);
+
+  }
+});
+
+// Items + SVG
+gsap.utils.toArray('.item').forEach(item => {
+  const viewportHeight = window.innerHeight;
+
+  // Animation fade + translate
+  gsap.fromTo(item, 
+    { opacity: 0, y: 400 }, 
+    { 
+      opacity: 1, 
+      y: 0, 
+      scrollTrigger: {
+        trigger: item,
+        start: () => 'top-=200 bottom',
+        end: () => 'top+=100 bottom',
+        markers: false,
+        scrub: true
+      } 
+    }
+  );
+
+  // === Animation du SVG avec DrawSVG ===
+  let paths = $(item).find('svg path');
+
+    let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: $(item).find('.svgText'), // ✅ le trigger est l'élément global
+        start: () => `top+=${0.1 * viewportHeight} bottom`,
+        end: () => `top+=${0.5 * viewportHeight} bottom`,
+        scrub: true,
+        markers: true
+    }
+    });
+
+    // reset
+    gsap.set(paths, { drawSVG: "0%" });
+
+    // séquence
+    tl.to(paths.eq(0), { drawSVG: "100%", ease: "power1.inOut" })
+    .to(paths.eq(1), { drawSVG: "100%", ease: "power1.inOut" },)
+    .to(paths.eq(2), { drawSVG: "100%", ease: "power1.inOut" },);
+    
+});
